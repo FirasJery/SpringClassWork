c4551a6ea52dfec9917b7cc6b8db90b8dd565a25
firasjery <firas.eljary@esprit.tn>
2023-10-31 19:22:26 +0100

    Controllers in progress

--- src/main/java/tn/esprit/firaseljary/services/IMoniteurService.java
+++ src/main/java/tn/esprit/firaseljary/services/IMoniteurService.java
@@ -4,6 +4,6 @@ import tn.esprit.firaseljary.entitites.Moniteur;
 
 public interface IMoniteurService {
 
-
+    Moniteur addInstructor(Moniteur moniteur);
     Moniteur addInstructorAndAssignToCourse(Moniteur moniteur, int numCourse);
 }
--- src/main/java/tn/esprit/firaseljary/services/MoniteurService.java
+++ src/main/java/tn/esprit/firaseljary/services/MoniteurService.java
@@ -7,17 +7,28 @@ import tn.esprit.firaseljary.entitites.Moniteur;
 import tn.esprit.firaseljary.repositories.CoursRepository;
 import tn.esprit.firaseljary.repositories.MoniteurRepository;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class MoniteurService implements IMoniteurService{
     private final MoniteurRepository moniteurRepository ;
     private final CoursRepository coursRepository ;
+
+    @Override
+    public Moniteur addInstructor(Moniteur moniteur) {
+        return this.moniteurRepository.save(moniteur);
+    }
+
     @Override
     public Moniteur addInstructorAndAssignToCourse(Moniteur moniteur, int numCourse) {
         Cours cours = coursRepository.findById(numCourse).orElse(null);
         if (cours != null)
         {
-            moniteur.getCoursSet().add(cours);
+            Set<Cours> edited = new HashSet<>();
+            edited.add(cours);
+            moniteur.setCoursSet(edited);
             return this.moniteurRepository.save(moniteur);
         }
 
